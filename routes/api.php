341ba20e19d94f81ed6cc8b211b1c06ca06b398e
mmolano <miike.molano@gmail.com>
2023-01-20 13:42:13 +0900

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'prefix' => '/api/v1.0',
+], function () {
+    include 'versioning/v1.0.php';
+});
